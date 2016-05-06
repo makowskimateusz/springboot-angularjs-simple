b927035e8b7f67edaf03b1a246f6ec080177beba
Mateusz Makowski <mateusz.makos@gmail.com>
2016-05-06 14:04:44 +0200

    Add test which check if meeting method is returing correct value

--- src/test/java/com/example/MakosSpringBootGetingStartedApplicationTests.java
+++ src/test/java/com/example/MakosSpringBootGetingStartedApplicationTests.java
@@ -1,11 +1,19 @@
 package com.example;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Boot.class)
@@ -15,20 +23,17 @@ public class MakosSpringBootGetingStartedApplicationTests {
     private MockMvc mock;
 
 
-//    @Before
-//    public void seetingUpMockMvcBuilder(){
-//
-//        mock = MockMvcBuilders.standaloneSetup(new MeetingController()).build();
-//
-//
-//    }
+    @Before
+    public void setingUpMockMvcBuilder(){
+
+        mock = MockMvcBuilders.standaloneSetup(new MeetingController()).build();
+
+
+    }
 
 
 //	@Test
 //	public void contextLoads() throws Exception{
-//
-//        Meeting meetingExpected = new Meeting("opis", "dzsiaj");
-//
 //
 //        mock.perform(MockMvcRequestBuilders.get("/add").accept(MediaType.APPLICATION_JSON))
 //                .andExpect(status().isOk());
@@ -37,8 +42,12 @@ public class MakosSpringBootGetingStartedApplicationTests {
 //	}
 
     @Test
-    public void testJson() {
+    public void testJson() throws Exception {
 
+        mock.perform(MockMvcRequestBuilders.get("/meeting")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.description", Matchers.is("jakieś tam spotkanie")));
 
     }
 
