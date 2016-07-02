a8e1b39f371834dfb6d585343c1926718c6a5519
Mateusz Makowski <mateusz.makos@gmail.com>
2016-07-02 16:14:55 +0200

    added comment how to test unit or integration

--- src/test/java/com/example/Tests.java
+++ src/test/java/com/example/Tests.java
@@ -4,15 +4,16 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,21 +23,19 @@ public class Tests {
 
     private MockMvc mock;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Before
     public void setingUpMockMvcBuilder(){
 
-        mock = MockMvcBuilders.standaloneSetup(new MeetingController()).build();
-
-
-    }
-
-    @Test
-    public void testJson() throws Exception {
-
-        mock.perform(get("/meeting")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.description", Matchers.is("jakieś tam spotkanie")));
+//        test jednostkowy, bo odwolujemy się bezposrednio do controllera
+//        aby moglbyć to test jednostkowy trzebaby bylo zrobic konekts do testow
+//        i wrzucic obiekt meetingControllera jako obiekt autowirowany
+//        wtedy autowriowałby się takze ten obiekt ktory jest meetinggeneratorem
+//        mock = MockMvcBuilders.standaloneSetup(new MeetingController()).build();
+//        test ingegracyjny, bo podnosimy kontekst aplikacji bez sprawdzania poszczegolnych metod
+          mock = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
     }
 
